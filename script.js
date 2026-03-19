32b954122242a133655aedfbf00cc90c90dbd022
23489ac <23489ac@hvhs.school.nz>
2026-03-19 23:29:57 +0000

    change

--- script.js
+++ script.js
@@ -10,27 +10,43 @@
 function setup() {
 	cnv = new Canvas(850, 850);
 	world.gravity.y = 10;
-	rectangle= new Sprite(212, 190, 217, 117, 'd');
-    rectangle.color = 'green';
-    rectangle.rotationSpeed = 2;
-    
+	bird= new Sprite(width/8, height/4, 80, 'd');
+    bird.color = 'blue';
+    bird.rotationSpeed = 2;
+    bird.vel.x = 2;
+ 
+    for (i = 0; i <4; i++) 
+    pipe1 = new Sprite(600,150,70,300,'k');
+    pipe1.color = 'green'
+    pipe1.vel.x = 1;
+    pipe1.bounciness = 1;
+    pipe1.friction = 0;
+    pipe2 =new Sprite(50,700,70,300,'k');
+    pipe2.color = 'green'
+    pipe2.vel.x = 1;
+    pipe3 =new Sprite(700,700,70,300,'k');
+    pipe3.color = 'green'
+    pipe3.vel.x = 1;
+    pipe4 =new Sprite(700,200,70,400,'k');
+    pipe4.color = 'green' 
+    pipe4.vel.x = 1;
+    pipe5 =new Sprite(500,700,70,300,'k');
+    pipe5.color = 'green'
+    pipe5.vel.x = 1;
+
+
+    }
+
 console.log("setup: ");
 
-}
 	
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
 	background('lightblue');
-    if ( keyboard.pressing('left')){
-        //set sprite's velocity to the left
-    }
-    else if ( keyboard.pressing('right')) {    
-};
-
-if ( keyboard.released('left')) {
-    // Set sprite's velocity to zero
+if (kb.presses('space')){
+    bird.vel.y= -5
 }
 
 	
