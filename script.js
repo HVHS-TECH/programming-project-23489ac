f0762710e87d436750d95f1ea99405fc65298dc4
23489ac <23489ac@hvhs.school.nz>
2026-03-26 22:41:46 +0000

    change

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // Sprite falls due to gravity
 // Written by ???
 /*******************************************************/
-	
+
 /*******************************************************/
 // setup()
 /*******************************************************/
@@ -12,42 +12,47 @@ function setup() {
 	world.gravity.y = 10;
 	bird= new Sprite(100,400, 80, 'd');
     bird.color = 'blue';
-    bird.vel.x = 2;
+    bird.vel.x= 0
+
  
-    pipe1Top = new Sprite(200, 100, 80, 300, 'k'); 
+    pipe1Top = new Sprite(300, 100, 80, 350, 'k'); 
     pipe1Top.color = 'green'; 
-    pipe1Top.vel.x = 4;
-    pipe1Bottom = new Sprite(200, 750, 80, 250, 'k'); 
+    pipe1Top.vel.x = -3;
+    pipe1Bottom = new Sprite(300, 750, 80, 550, 'k'); 
     pipe1Bottom.color = 'green'; 
-    pipe1Bottom.vel.x = 4;
+    pipe1Bottom.vel.x = -3;
 
-    pipe2Top = new Sprite(350, 120, 80, 280, 'k'); 
+    pipe2Top = new Sprite(550, 120, 80, 280, 'k'); 
     pipe2Top.color = 'green'; 
-    pipe2Top.vel.x = 1;
-    pipe2Bottom = new Sprite(350, 750, 80, 300, 'k'); 
+    pipe2Top.vel.x = -3;
+    pipe2Bottom = new Sprite(550, 750, 80, 300, 'k'); 
     pipe2Bottom.color = 'green'; 
-    pipe2Bottom.vel.x = 1;
+    pipe2Bottom.vel.x = -3;
 
-     pipe3Top = new Sprite(500, 90, 80, 380, 'k'); 
+    pipe3Top = new Sprite(800, 90, 80, 380, 'k'); 
     pipe3Top.color = 'green'; 
-    pipe3Top.vel.x = 1;
-    pipe3Bottom = new Sprite(500, 770, 80, 250, 'k'); 
+    pipe3Top.vel.x = -3;
+    pipe3Bottom = new Sprite(800, 770, 80, 250, 'k'); 
     pipe3Bottom.color = 'green'; 
-    pipe3Bottom.vel.x = 1;
+    pipe3Bottom.vel.x = -3;
 
-    pipe4Top = new Sprite(650, 150, 80, 300, 'k'); 
+    pipe4Top = new Sprite(1050, 150, 80, 300, 'k'); 
     pipe4Top.color = 'green'; 
-    pipe4Top.vel.x = 1;
-    pipe4Bottom = new Sprite(650, 700, 80, 280, 'k'); 
+    pipe4Top.vel.x = -3;
+    pipe4Bottom = new Sprite(1050, 700, 80, 280, 'k'); 
     pipe4Bottom.color = 'green'; 
-    pipe4Bottom.vel.x = 1;
+    pipe4Bottom.vel.x = -3;
 
-    pipe5Top = new Sprite(800, 100, 80, 320, 'k'); 
+    pipe5Top = new Sprite(1300, 100, 80, 320, 'k'); 
     pipe5Top.color = 'green'; 
-    pipe5Top.vel.x = 1;
-    pipe5Bottom = new Sprite(800, 750, 80, 250, 'k'); 
+    pipe5Top.vel.x = -3;
+    pipe5Bottom = new Sprite(1300, 750, 80, 250, 'k'); 
     pipe5Bottom.color = 'green'; 
-    pipe5Bottom.vel.x = 1;
+    pipe5Bottom.vel.x = -3;
+
+   
+
+
 
 
     }
@@ -59,11 +64,29 @@ console.log("setup: ");
 // draw()
 /*******************************************************/
 function draw() {
-	background('lightblue');
-if (kb.presses('space')){
-    bird.vel.y= -7
+	background('lightblue')
+    
 
+    if(started == false){ 
+
+    }
+    textSize(60);
+    fill ('yellow');
+    textAlign(CENTER, CENTER);
+    text('FLAPPY BIRD',400, 300);
+
+    rect(300,250,70,10);
+    fill('green');
+    textSize (35);
+    fill('white');
+    text(START , 400,400)
+    if (mouse.presses()) {
+			return;
+    }
+if (kb.presses('space')){
+     bird.vel.y= -7;
 }
+     
 }
 
 
